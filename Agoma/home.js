3ad29bb451eb9184ae6a0722d4f91d4562bc49b6
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-06 22:01:19 +0530

    6th November

---
+++ Agoma/home.js
@@ -0,0 +1,11 @@
+// Declarations
+const nameInput = document.getElementById("fullnameInput").value
+const aliasInput = document.getElementById("aliasInput").value
+const submitBtn = document.querySelector(".submitBtn")
+
+
+
+submitBtn.addEventListener( "click" , (e)=>{
+    e.preventDefault();
+
+} )
