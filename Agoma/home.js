470079015680de507deee93512b48741e528d316
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-07 20:56:37 +0530

    7th November

--- Agoma/home.js
+++ Agoma/home.js
@@ -2,9 +2,6 @@
 const nameInput = document.getElementById("fullnameInput").value
 const aliasInput = document.getElementById("aliasInput").value
 const submitBtn = document.querySelector(".submitBtn")
-
-
-
 submitBtn.addEventListener( "click" , (e)=>{
     e.preventDefault();
 
