a3c9b94be6df6e1b753059054a30f5da21ac23db
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-09 07:42:59 +0530

    Session Storage

--- Agoma/home.js
+++ Agoma/home.js
@@ -3,6 +3,7 @@ let nameInput = document.getElementById("fullnameInput")
 let aliasInput = document.getElementById("aliasInput")
 const submitBtn = document.querySelector(".submitBtn")
 const alertDiv = document.querySelector(".alert")
+let aliasValue = null;
 
 // Alert Functions
 function alert( text , color , color2 , symbol ){
@@ -24,37 +25,43 @@ function alert( text , color , color2 , symbol ){
     } , 4000 )
 }
 
-// automated value of alias
-// if the input value is filled then only
-nameInput.addEventListener( "change" , () => {
-    // split the value in two parts First name and Last name, add First name in alias
-    const aliasValue = nameInput.value.split(" ")[0]
-    aliasInput.value = aliasValue
-} )
+// if following values are null, dont run this part.
+if( nameInput != null && submitBtn != null ){
 
-// Submit Button 
-submitBtn.addEventListener( "click" , (e)=>{
-    e.preventDefault();
-    // Trim the input value
-    nameInput.value = nameInput.value.trim()
-    // Checking for edge cases if value is null, empty, or value consist number
-    const regexInt = /\d/
-    if( nameInput.value == null || nameInput.value == "" ){
-        alert( "Enter your name", "#C62828" , "#EF9A9A" , "fa-solid fa-exclamation" )
-    } 
-    else if( regexInt.test(nameInput.value) ){
-        alert( "Enter valid name", "#C62828" , "#EF9A9A" , "fa-solid fa-hashtag" )
-    }
-    else if( aliasInput.value == null || aliasInput.value == "" ){
-        alert( "Enter Alias" , "#C62828" , "#EF9A9A" , "fa-solid fa-shield-halved" )
-    }
-    else if( regexInt.test(aliasInput.value) ){
-        alert( "Enter valid alias", "#C62828" , "#EF9A9A" , "fa-solid fa-hashtag" )
-    }
-    else{
-        alert(`Thanks, ${aliasInput.value}` , '#673ab7' , "transparent" , "fa-regular fa-chess-king")
-        setTimeout( ()=>{
-            window.location.href = "mainPage.html"
-        } , 2000 )
-    }
-} )
+    // automated value of alias
+    // if the input value is filled then only
+    nameInput.addEventListener( "change" , () => {
+        // split the value in two parts First name and Last name, add First name in alias
+        aliasValue = nameInput.value.split(" ")[0]
+        aliasInput.value = aliasValue
+        console.log(aliasValue)
+    } )
+    
+    // Submit Button 
+    submitBtn.addEventListener( "click" , (e)=>{
+        e.preventDefault();
+        // Trim the input value
+        nameInput.value = nameInput.value.trim()
+        // Checking for edge cases if value is null, empty, or value consist number
+        const regexInt = /\d/
+        if( nameInput.value == null || nameInput.value == "" ){
+            alert( "Enter your name", "#C62828" , "#EF9A9A" , "fa-solid fa-exclamation" )
+        } 
+        else if( regexInt.test(nameInput.value) ){
+            alert( "Enter valid name", "#C62828" , "#EF9A9A" , "fa-solid fa-hashtag" )
+        }
+        else if( aliasInput.value == null || aliasInput.value == "" ){
+            alert( "Enter Alias" , "#C62828" , "#EF9A9A" , "fa-solid fa-shield-halved" )
+        }
+        else if( regexInt.test(aliasInput.value) ){
+            alert( "Enter valid alias", "#C62828" , "#EF9A9A" , "fa-solid fa-hashtag" )
+        }
+        else{
+            alert(`Thanks, ${aliasInput.value}` , '#673ab7' , "#ede7f6" , "fa-regular fa-chess-king")
+            sessionStorage.setItem("alias",aliasInput.value)
+            setTimeout( ()=>{
+                window.location.href = "mainPage.html"
+            } , 2000 )
+        }
+    } )
+}
---
+++ Agoma/mainpage.js
@@ -0,0 +1,4 @@
+
+const heading = document.createElement("h1");
+heading.textContent = sessionStorage.getItem("alias");
+document.body.appendChild(heading);
