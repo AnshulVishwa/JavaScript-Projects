caebf864105cf73c2c26acc8d509428e7fa7a7bd
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-09 20:04:37 +0530

    mainpage css

--- Agoma/mainpage.js
+++ Agoma/mainpage.js
@@ -1,7 +1,22 @@
 const greet = document.querySelector(".greet")
 const span = document.createElement("span")
 const allListContent = document.querySelectorAll(".icon")
+const previousDocs = document.querySelector(".previousDocs")
 
+let totalDocs = 10
+
+for( let i = 0 ; i < totalDocs ; i++ ){
+    const create = document.createElement("div")
+    create.classList.add("Docs")
+    const cover = document.createElement("div")
+    cover.classList.add("coverPage")
+    const title = document.createElement("div")
+    title.classList.add("title")
+    title.textContent = `Doc ${i+1}`
+    create.appendChild(cover)
+    create.appendChild(title)
+    previousDocs.appendChild(create)
+}
 allListContent.forEach( (each) => {
     each.parentElement.addEventListener( "mouseover" , () => {
         each.id = "rotate"
