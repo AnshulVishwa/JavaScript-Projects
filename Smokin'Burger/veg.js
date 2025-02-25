c613559175afdd9d74199f0d4401a3d11fdcb358
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-02-25 20:20:53 +0530

    Fewer Updates

--- Smokin'Burger/veg.js
+++ Smokin'Burger/veg.js
@@ -1,3 +1,12 @@
+// Cart Intersection
+const observer = new IntersectionObserver( ( entries ) => {
+    entries.forEach( (each , i) => {
+        if( each.boundingClientRect.left == 1488 ){
+            console.log(each)
+        }
+    } ) 
+} )
+
 // Cart Option
 const number = document.querySelector(".number")
 const cart = document.querySelector(".cart-option")
@@ -6,17 +15,26 @@ let length = JSON.parse(localStorage.getItem( "cart_items" )).length
 number.textContent = length
 
 function createElement( cart_values ){
-    console.log(cart_values)
     const create = document.createElement("div")
     create.classList.add("cart_box")
-    cart_values.forEach( ( each ) => {    
+    const iconDiv = document.createElement("div")
+    iconDiv.classList.add("icon_div")
+    create.appendChild(iconDiv)
+    
+    let SumPrice = cart_values.reduce((sum, v) => sum + parseFloat(v.price.replace("$", "")), 0);
+
+    cart_values.forEach( ( each , i ) => { 
+        SumPrice += parseFloat(each.price.split("$")[1])   
         const boxInside = document.createElement("div")
+        observer.observe(boxInside)
         boxInside.classList.add("boxInside")
-        const burgerName = document.createElement("h2")
+        boxInside.id = `box${i+1}`
+        const burgerName = document.createElement("h3")
         burgerName.classList.add("burgerName")
-        burgerName.textContent = each.burger
+        burgerName.textContent = `Burger : ${each.burger}`
         const toppings = document.createElement("ul")
         if( each.toppings.length != 0 ){
+            toppings.innerHTML = "<b>Toppings :</b>"
             each.toppings.map( ( v ) => {
                 const list = document.createElement("li")
                 list.textContent = v
@@ -25,14 +43,19 @@ function createElement( cart_values ){
         }
         const quantity = document.createElement("span")
         quantity.classList.add("quantity")
-        quantity.textContent = each.quantity
+        quantity.textContent = `Quantity : ${each.quantity}`
         const price = document.createElement("span")
         price.classList.add("price")
-        price.textContent = each.price
+        price.textContent = `Price : ${each.price}`
         cart.appendChild(create)
         create.appendChild(boxInside)
         boxInside.append(burgerName , toppings , quantity , price)
     } )
+    for( let i = 0 ; i < cart_values.length ; i++ ){
+        const icon = document.createElement("span")
+        icon.innerHTML = `<i id="icon${i+1}" class="fa-solid fa-circle"></i>`
+        iconDiv.appendChild(icon)
+    }
 }
 
 let showCart = false;
